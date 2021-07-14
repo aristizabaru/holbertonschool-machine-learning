26b998ce78445987ff40dba19cffb97ba258aa57
USUARIO <usuario@MacBook-Pro-de-USUARIO.local>
2021-07-14 14:50:55 -0500

    first commit

---
+++ math/0x00-linear_algebra/6-howdy_partner.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Concatenates two array"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenate two array
+
+    Args:
+        arr1 (list): list of integer/floats
+        arr2 (list): list of integer/floats
+
+    Returns:
+        list: arr1 concatenated with arr2
+    """
+    return arr1 + arr2
