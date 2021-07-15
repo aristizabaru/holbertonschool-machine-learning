a69c00ed1a5efb43ad60c0e5800069503db2f9a2
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-15 07:10:09 -0500

    first commit

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Slices a matrix along specific axes"""
+
+
+def np_slice(matrix, axes={}):
+    """Slices a matrix along specific axes
+    The if no axe is passed along the dictonary
+    the default value will be `None`
+
+    Args:
+        matrix (numpy.ndarray): matrix to be sliced
+        axes (dict, optional): slices. Defaults to {}.
+
+    Returns:
+        numpy.ndarray: sliced matrix
+    """
+    slices = []
+    for i in range(len(matrix.shape)):
+        if i in axes:
+            # unpack elements to slice function
+            slices.append(slice(*axes[i]))
+        else:
+            slices.append(slice(None, None, None))
+    # Using a non-tuple sequence for multidimensional indexing is deprecated
+    return matrix[tuple(slices)]
