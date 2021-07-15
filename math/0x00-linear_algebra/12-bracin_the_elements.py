128597c2e15ea2937e75917d143b95f839f5c90c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-14 21:09:48 -0500

    docs: trim docstring length

--- math/0x00-linear_algebra/12-bracin_the_elements.py
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python3
-"""Performs element-wise addition, subtraction, multiplication, and division"""
+"""Performs element-wise addition,
+subtraction, multiplication, and division"""
 
 
 def np_elementwise(mat1, mat2):
-    """Performs element-wise addition, subtraction, multiplication, and division
+    """Performs element-wise addition, subtraction,
+    multiplication, and division
 
     Args:
         mat1 (numpy.ndarray): matrix A
         mat2 (numpy.ndarray): matrix B
 
     Returns:
-        tuple: element-wise sum, difference, product, and quotient, respectively
+        tuple: element-wise sum, difference, product,
+                and quotient, respectively
     """
     return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
