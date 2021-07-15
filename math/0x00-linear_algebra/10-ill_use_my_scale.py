609942ea925074b7771769a01951930ba79b3338
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-14 20:24:08 -0500

    first commit

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Calculates the shape of a numpy.ndarray"""
+
+
+def np_shape(matrix):
+    """Calculates the shape of a numpy.ndarray
+
+    Args:
+        matrix (numpy.ndarray): matrix to be evaluated
+
+    Returns:
+        tuple: describe the shape of the array
+    """
+    return matrix.shape
