cb432f8b22ad16c2bf030ff7f2aceb6c62aeae21
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-14 20:30:28 -0500

    first commit

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Transposes a matrix"""
+
+
+def np_transpose(matrix):
+    """Transposes a matrix
+
+    Args:
+        matrix (numpy.ndarray): matrix to be transposed
+
+    Returns:
+        numpy.ndarray: transpose matrix
+    """
+    return matrix.transpose()
