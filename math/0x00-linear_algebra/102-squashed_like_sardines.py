2a3820f0eeeb60bc7da210ce8801a60fdd81062f
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-15 20:08:39 -0500

    first commit

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -1,10 +1,42 @@
 #!/usr/bin/env python3
-"""Adds two matrices"""
+"""Concatenates two matrices along a specific axis"""
 
 
 def cat_matrices(mat1, mat2, axis=0):
-    pass
+    """Concatenates two matrices along a specific axis
+
+    Args:
+        mat1 (list): matrix of int/floats
+        mat2 list): matrix of int/floats
+        axis (int, optional): axis to perform concatenation. Defaults to 0.
+
+    Returns:
+        list: concatenated matrix. None if it can't be done
+    """
+
+    if matrix_shape(mat1) != matrix_shape(mat2):
+        return
+
+    if not axis:
+        return mat1 + mat2
+    # check if rows are equals
+    elif axis == 1:
+        return [mat1[row] + mat2[row] for row in range(len(mat1))]
 
 
-# validate axis
-# if axis 0
+def matrix_shape(matrix):
+    """Calculates the shape of a matrix with the same dimension
+
+    Args:
+        matrix (list): matrix to be evaluated
+
+    Returns:
+        shape_matrix (list): list of integers desribing the matrix shape
+    """
+    # base case
+    if type(matrix) is int:
+        return []
+
+    shape_matrix = matrix_shape(matrix[0])
+    shape_matrix.insert(0, len(matrix))
+    return shape_matrix
