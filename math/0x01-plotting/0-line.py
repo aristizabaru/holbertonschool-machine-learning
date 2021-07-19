e88cf1f9641f3aa85057fb02afcf7a1d37a7d08a
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-19 15:13:10 -0500

    first commit

---
+++ math/0x01-plotting/0-line.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+y = np.arange(0, 11) ** 3
+
+
+plt.plot(y, color='r')
+# adjust the limits of the axis
+plt.xlim(left=0, right=10)
+plt.show()
