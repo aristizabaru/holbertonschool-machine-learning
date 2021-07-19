e23d3a5757e12517e148e17773291452c4ac39e9
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-19 17:34:51 -0500

    first commit

---
+++ math/0x01-plotting/3-two.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 21000, 1000)
+r = np.log(0.5)
+t1 = 5730
+t2 = 1600
+y1 = np.exp((r / t1) * x)
+y2 = np.exp((r / t2) * x)
+
+plt.plot(x, y1, color='r', linestyle='--', label='C-14')
+plt.plot(x, y2, color='g', label='Ra-226')
+# set limits
+plt.xlim(left=0, right=20000)
+plt.ylim(bottom=0, top=1)
+# show legend
+plt.legend()
+# set labels
+plt.xlabel('Time (years)')
+plt.ylabel('Fraction Remaining')
+plt.title('Exponential Decay of Radioactive Elements')
+plt.show()
