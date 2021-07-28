8943fbdd1a21e45ba737f635a5a3a7488e4c71cf
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-28 15:02:01 -0500

    first commit

---
+++ math/0x03-probability/README.md
@@ -0,0 +1,63 @@
+# 0x03. Probability
+
+## About
+
+This is an educational project exploring concepts about **Probability** towards a formation in Machine Learning.
+
+## Table of contents
+
+- [Requirements](#requirements)
+  - [Dependencies](#dependencies)
+- [Topics](#topics)
+- [Read or watch](#read-or-watch)
+- [Files](#files)
+  - [0. Sigma is for Sum](#0-sigma-is-for-sum)
+  - [1. The Greeks pronounce it sEEgma](#1-The-greeks-pronounce-it-seegma)
+  - [2. Pi is for Product](#2-pi-is-for-product)
+  - [3. The Greeks pronounce it pEE](#3-the-greeks-pronounce-it-pee)
+  - [4. Hello, derivatives!](#4-hello-derivatives)
+  - [5. A log on the fire](#5-a-log-on-the-fire)
+  - [6. It is difficult to free fools from the chains they revere](#6-it-is-difficult-to-free-fools-from-the-chains-they-revere)
+  - [7. Partial truths are often more insidious than total falsehoods](#7-partial-truths-are-often-more-insidious-than-total-falsehoods)
+  - [8. Put it all together and what do you get?](#8-put-it-all-together-and-what-do-you-get)
+  - [9. Our life is the sum total of all the decisions we make every day, and those decisions are determined by our priorities](#9-our-life-is-the-sum-total-of-all-the-decisions-we-make-every-day-and-those-decisions-are-determined-by-our-priorities)
+  - [10. Derive happiness in oneself from a good day's work](#10-derive-happiness-in-oneself-from-a-good-days-work)
+  - [11. Good grooming is integral and impeccable style is a must](#11-good-grooming-is-integral-and-impeccable-style-is-a-must)
+  - [12. We are all an integral part of the web of life](#12-we-are-all-an-integral-part-of-the-web-of-life)
+  - [13. Create a definite plan for carrying out your desire and begin at once](#13-create-a-definite-plan-for-carrying-out-your-desire-and-begin-at-once)
+  - [14. My talents fall within definite limitations](#14-my-talents-fall-within-definite-limitations)
+  - [15. Winners are people with definite purpose in life](#15-winners-are-people-with-definite-purpose-in-life)
+  - [16. Double whammy](#16-double-whammy)
+  - [17. Integrate](#17-integrate)
+
+## Requirements
+
+- Python 3.6.14
+
+### Dependencies
+
+Please refer at the [requirements.txt](../../requirements.txt) file at the root of this repository.
+
+## Topics
+
+- Summation and Product notation
+- What is a series?
+- Common series
+- What is a derivative?
+- What is the product rule?
+- What is the chain rule?
+- Common derivative rules
+- What is a partial derivative?
+- What is an indefinite integral?
+- What is a definite integral?
+- What is a double integral?
+
+## Read or watch
+
+Before start please read or watch this concepts.
+
+**Extra**
+
+## Files
+
+This project (lesson) is conceived to be carried out step by step, that is why the description of the files is presented as a statement.
---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+
+class Poisson:
+    """Represents poisson distribution.
+
+    Def. A discrete frequency distribution which gives
+    the probability of a number of 'independent events'
+    occurring in a fixed time.
+
+    Probability Mass Function (PMF):
+        This function gives the probability of observing 'k'
+        events in defined interval, the average of the
+        events in the same interval.
+
+        Formula → f(k,λ) = P(X = k) = (e^-λ * λᴷ) / k!
+        k → # of events in a interval
+        λ → avarage of events in a interval.
+            Must always be positive
+        e → 2.7182818285 (Euler's number)
+
+    Cumulative Distribution Function (CDF)
+        This functions gives the probability from a variable
+        to be less or equal than a given value
+
+        General Formula → F(x) = P(X ≤ x)
+
+        Probability Distribution Formula:
+        F(x) = Σ pmf(i) from i=0 to x
+        x → upper limit
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        """Init Poisson instance with a lambtha
+        public attribute
+
+        Expl:
+            lambtha is the expected number of events
+            in a interval (mean). λ has to be always > 0
+
+            Formula → λ = Σ xᵢ / N
+            Σ xᵢ → Sum of events in a interval
+            N → # of events in a interval
+
+        Args:
+            data (list, optional): events. Defaults to None.
+
+            lambtha (float, optional): average of events. Defaults to 1.0
+        """
+        if data is None:
+            self.lambtha = float(lambtha)
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+        else:
+            # λ = Σ xᵢ / N
+            self.lambtha = float(sum(data) / len(data))
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+
+    @staticmethod
+    def factorial(num):
+        """Computes a factorial number recursively
+
+        Args:
+            num (int): factorial number
+
+        Returns:
+            int: computed factorial number
+        """
+        return num * Poisson.factorial(num - 1) if num > 1 else 1
+
+    def pmf(self, k):
+        """Probability Mass Function:
+        Computes the probability of observing 'k'
+        events in defined interval, the average of the
+        events in the same interval.
+
+        Args:
+            k (int/float): # of events in a interval
+
+        Returns:
+            float: probability of k's event success
+        """
+        e = 2.7182818285  # Euler's number
+        k = int(k)
+        λ = self.lambtha
+
+        if k < 0:
+            return 0
+        # P(X = k) = (e^-λ * λᴷ) / k!
+        return (e ** -λ * λ ** k) / Poisson.factorial(k)
+
+    def cdf(self, x):
+        """Cumulative Distribution Function:
+        This functions gives the probability from a variable
+        to be less or equal than a given value
+
+        General Formula → F(x) = P(X ≤ x)
+
+        Probability Distribution Formula:
+        F(x) = Σ pmf(i) from i=0 to x
+
+        Args:
+            x (int/float): x → upper limit
+
+        Returns:
+            float: cumulative probability
+        """
+        x = int(x)
+
+        if x < 0:
+            return 0
+        # Σ pmf(i) from i=0 to x
+        return sum([self.pmf(i) for i in range(x + 1)])
