079627b516948ec735c0f1c44a4145fc4ecd864a
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-28 20:19:12 -0500

    first commit

---
+++ math/0x03-probability/normal.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This module represents Normal distribution"""
+
+
+class Normal:
+    """docstring"""
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """docstring"""
+
+        if data is None:
+            self.mean = float(mean)
+            self.stddev = float(stddev)
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+
+            n = len(data)
+            # μ = Σ xᵢ / n
+            self.mean = sum(data) / n
+            # variance → σ² = Σ (x - μ)² / n
+            variance = sum([(x - self.mean) ** 2 / n for x in data])
+            # σ = √σ²
+            self.stddev = variance ** (1/2)
