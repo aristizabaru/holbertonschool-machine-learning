0fc750049de0e07ebb505ca2ff5fad0e28653c91
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-07-28 22:58:23 -0500

    fix: checks adding <= or >= according the cas

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -9,16 +9,16 @@ class Binomial:
         """docstring"""
 
         if data is None:
-            if n < 0:
-                raise ValueError("n must be a positive value")
-            if p < 0 or p > 1:
-                raise ValueError("p must be greater than 0 and less than 1")
+            if n <= 0:
+                raise ValueError('n must be a positive value')
+            if p <= 0 or p >= 1:
+                raise ValueError('p must be greater than 0 and less than 1')
             self.n = int(n)
             self.p = float(p)
         else:
             if not isinstance(data, list):
                 raise TypeError('data must be a list')
-            if len(data) < 2:
+            if len(data) <= 2:
                 raise ValueError('data must contain multiple values')
 
             n = len(data)
@@ -28,4 +28,4 @@ class Binomial:
             variance = sum([(x - μ) ** 2 / n for x in data])
             self.p = 1 - (variance / μ)
             self.n = int(round(μ / self.p))
-            self.p = μ / self.n
+            self.p = float(μ / self.n)
