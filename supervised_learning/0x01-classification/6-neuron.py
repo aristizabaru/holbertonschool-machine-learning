2a36f74f37e929410f168882adb5cf8a6fbdf59c
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-08 14:22:20 -0500

    fix: iterations and alpha ValueError are fired when they are < 1

--- supervised_learning/0x01-classification/6-neuron.py
+++ supervised_learning/0x01-classification/6-neuron.py
@@ -189,11 +189,11 @@ class Neuron:
         """
         if not isinstance(iterations, int):
             raise TypeError('iterations must be an integer')
-        if iterations < 0:
+        if iterations < 1:
             raise ValueError('iterations must be a positive integer')
         if not isinstance(alpha, float):
             raise TypeError('alpha must be a float')
-        if alpha < 0:
+        if alpha < 1:
             raise ValueError('alpha must be positive')
 
         for i in range(iterations):
