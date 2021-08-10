d22f729305e09485be3a08a9f32dd6226273838d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-10 15:45:08 -0500

    first commit

---
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""one_hot_decode module"""
+
+import numpy as np
+
+
+def one_hot_decode(one_hot):
+    """converts a one-hot matrix into a vector of label
+
+    Args:
+        one_hot (numpy.ndarray):
+            one-hot encoded numpy.ndarray with shape (classes, m)
+
+    Returns:
+        numpy.ndarray:
+            numeric labels for each example, or None on failure
+    """
+    # returns the indices of the maximum values along an axis
+    one_hot_decode = np.argmax(one_hot, axis=0)
+    return one_hot_decode
