88f5b6a62213586205f110d681bd659accc6971e
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-10 05:22:16 -0500

    style: move check of layer[0] next to initials checks

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -47,10 +47,10 @@ class DeepNeuralNetwork:
             raise ValueError('nx must be a positive integer')
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
-
         # check if layer[0] in list is integer
         if layers[0] < 1 or not isinstance(layers[0], int):
             raise TypeError('layers must be a list of positive integers')
+
         self.L = len(layers)
         self.cache = dict()
         self.weights = dict()
