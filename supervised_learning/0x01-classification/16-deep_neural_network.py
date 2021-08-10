3ca6017cd10021cee21916cba2f5ae05ff523d69
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-09 23:11:31 -0500

    refactor: reorganize checks for correct data type in layer list to go through all the items in the list

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -45,10 +45,12 @@ class DeepNeuralNetwork:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if not isinstance(layers, list) or len(layers) == 0\
-                or np.argmin(layers) < 0:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of positive integers')
 
+        # check if layer[0] in list is integer
+        if layers[0] < 1 or not isinstance(layers[0], int):
+            raise TypeError('layers must be a list of positive integers')
         self.L = len(layers)
         self.cache = dict()
         self.weights = dict()
@@ -59,8 +61,8 @@ class DeepNeuralNetwork:
         self.weights["b1"] = np.zeros([layers[0], 1], dtype=float)
         # initialize L2 and L3
         for layer in range(1, self.L):
-            # check if layer is int
-            if not isinstance(layers[layer], int):
+            # check if layer[layer] in list is integer
+            if layers[layer] < 1 or not isinstance(layers[layer], int):
                 raise TypeError('layers must be a list of positive integers')
             # He et al method
             # weight = np.random.randn(layer_size[l],layer_size[l-1])
@@ -68,7 +70,7 @@ class DeepNeuralNetwork:
             self.weights["W{}".format(
                 layer + 1)] = np.random.randn(layers[layer],
                                               layers[layer-1])*np.sqrt(
-                                                  2/layers[layer-1])
+                2/layers[layer-1])
             # bias (float of zero's x # neurons of the layer)
             self.weights["b{}".format(
                 layer + 1)] = np.zeros([layers[layer], 1], dtype=float)
