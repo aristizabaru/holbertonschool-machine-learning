fa5f2ff2ca68d66b1894441d38e84739a96e039d
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-09 22:50:42 -0500

    feat: add check if list is empty

--- supervised_learning/0x01-classification/16-deep_neural_network.py
+++ supervised_learning/0x01-classification/16-deep_neural_network.py
@@ -45,7 +45,8 @@ class DeepNeuralNetwork:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if not isinstance(layers, list) or np.argmin(layers) < 1:
+        if not isinstance(layers, list) or len(layers) == 0\
+                or np.argmin(layers) < 1:
             raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
