bee1a934f62b223d9ec6c5e0bfd0f0b0634bbe02
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-16 17:58:59 -0500

    docs: create docstring to funciton create_placeholders(nx, classes)

--- supervised_learning/0x02-tensorflow/0-create_placeholders.py
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -4,6 +4,16 @@ import tensorflow as tf
 
 
 def create_placeholders(nx, classes):
+    """returns two placeholders, x and y, for
+    the neural networ
+
+    Args:
+        nx (int): number of feature columns in our data
+        classes (int): number of classes in our classifier
+
+    Returns:
+        tuple: tenserflow placeholders named x and y
+    """
     x = tf.placeholder(tf.float32, shape=(None, nx), name='x')
     y = tf.placeholder(tf.float32, shape=(None, classes), name='y')
     return (x, y)
