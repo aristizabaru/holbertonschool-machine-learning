8ea570a8e84edf05a73a75272fa5b808e6d9f266
Andrés Aristizábal <andres.aristizabal@gmail.com>
2021-08-16 20:42:03 -0500

    chore: take answers as one hot encoded matrices, and find answers comparing correct labels and predictions

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -6,6 +6,13 @@ import tensorflow as tf
 def calculate_accuracy(y, y_pred):
     """calculates the accuracy of a prediction
 
+    Exp. 'y' and 'y_pred' are one hot encoded, tf.argmax()
+    creates new vectors with the indexes of the correct
+    asnwers (y) and predicted answers (y_pred).
+    tf.equal() evaluates both vecotors and return true/false
+    if there's a match and finally tf.reduce_mean() calculates
+    the average of correct answers.
+
     Args:
         y (tensorflow.python.framework.ops.Tensor):
             placeholder for the labels of the input data
@@ -16,7 +23,14 @@ def calculate_accuracy(y, y_pred):
         tensorflow.python.framework.ops.Tensor:
              tensor containing the decimal accuracy of the prediction
     """
-    equality = tf.equal(y, y_pred),
-    # Computes the mean of elements across dimensions of a tensor
-    mean = tf.reduce_mean(tf.cast(equality, 'float32'))
-    return mean
+    # new vector with the indexes of the correct asnwers
+    correct_answers = tf.argmax(y, 1)
+    # new vector with the indexes of the predicted asnwers
+    predicted_answers = tf.argmax(y, 1)
+    # evaluate if both answers match (true/false)
+    predictions = tf.equal(correct_answers, predicted_answers)
+    # compute the mean of the predictions vector
+    # casting bool values to floats
+    accuracy_percentage = tf.reduce_mean(tf.cast(predictions, "float"))
+
+    return accuracy_percentage
